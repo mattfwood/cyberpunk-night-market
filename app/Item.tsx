b81a9e7e71ebe69206c86f64e5bd4cd31edb450c
Matt Wood <matt@cratebind.com>
2023-03-03 15:51:03 -0600

    Extend colors, add hover colors

--- app/Item.tsx
+++ app/Item.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { Disclosure } from '@headlessui/react';
 import { ItemType } from './page';
 
@@ -5,14 +7,12 @@ const Item = ({ item }: { item: ItemType }) => {
   return (
     <Disclosure>
       <div key={item.name} className="p-4 mb-4">
-        <Disclosure.Button>
-          <div className="flex">
-            <div>
-              <h2 className="text-2xl font-bold">{item.name}</h2>
-              <p className="text-sm text-gray-500">{item.company}</p>
-            </div>
-            <p className="text-lg font-bold">${item.price}</p>
+        <Disclosure.Button className="flex justify-between w-full hover:bg-primary-200 transition-all py-2 px-1">
+          <div>
+            <h2 className="text-2xl font-bold">{item.name}</h2>
+            <p className="text-sm text-gray-500">{item.company}</p>
           </div>
+          <p className="text-lg font-bold">${item.price}</p>
         </Disclosure.Button>
         <Disclosure.Panel>
           <p className="text-sm">{item.description}</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       colors: {
-        primary: "#fd2133",
-        secondary: "#161616",
-        accent: "#ed8936",
-        "accent-light": "#f6ad55",
+        primary: {
+          DEFAULT: '#fd2133',
+          100: 'rgba(253,33,51,0.1)',
+          200: 'rgba(253,33,51,0.25)',
+          300: 'rgba(253,33,51,0.5)',
+        },
+        secondary: '#161616',
+        accent: '#ed8936',
+        'accent-light': '#f6ad55',
       },
     },
   },
   plugins: [],
-}
+};
