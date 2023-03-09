c155fdbda958356758bc345f40c2e783bf3e690f
Matt Wood <matt@cratebind.com>
2023-03-09 11:48:31 -0600

    Add item quantity modifying

--- app/Item.tsx
+++ app/Item.tsx
@@ -3,11 +3,16 @@
 import { Disclosure } from '@headlessui/react';
 import { ItemType } from './page';
 
-const Item = ({ item }: { item: ItemType }) => {
+type ItemProps = {
+  item: ItemType;
+  updateItemQuantity: (item: ItemType, quantity: number) => void;
+};
+
+const Item = ({ item, updateItemQuantity }: ItemProps) => {
   return (
     <Disclosure>
-      <div key={item.name} className="p-4 mb-4">
-        <Disclosure.Button className="flex justify-between w-full hover:bg-primary-200 transition-all py-2 px-1">
+      <div key={item.name} className="p-4 mb-4 flex">
+        <Disclosure.Button className="flex justify-between items-center w-full hover:bg-primary-200 transition-all p-2">
           <div>
             <h2 className="text-2xl font-bold">{item.name}</h2>
             <p className="text-sm text-gray-500">{item.company}</p>
@@ -17,6 +22,19 @@ const Item = ({ item }: { item: ItemType }) => {
         <Disclosure.Panel>
           <p className="text-sm">{item.description}</p>
         </Disclosure.Panel>
+        <div className="flex flex-col w-10 justify-center">
+          {/* <button>+</button> */}
+          <input
+            className="text-secondary"
+            type="number"
+            min={0}
+            value={item.quantity}
+            onChange={(e) => {
+              updateItemQuantity(item, parseInt(e.target.value));
+            }}
+          />
+          {/* <button>-</button> */}
+        </div>
       </div>
     </Disclosure>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useState } from 'react';
 import Item from './Item';
 
 export type ItemType = {
@@ -5,24 +8,64 @@ export type ItemType = {
   company?: string;
   description: string;
   price: number;
+  category: string;
+  quantity: number;
 } & { [key: string]: string };
 
-const items = [
+const defaultItems = [
   {
     name: 'Item 1',
     company: 'Arasaka',
     description: 'This is item 1',
     price: 100,
+    category: 'weapons',
+    quantity: 0,
   },
 ] as ItemType[];
 
 export default function Home() {
+  const [items, setItems] = useState<ItemType[]>(defaultItems);
+
+  const groupedItems = items.reduce((acc, item) => {
+    if (!acc[item.category]) {
+      acc[item.category] = [];
+    }
+    acc[item.category].push(item);
+    return acc;
+  }, {} as { [key: string]: ItemType[] });
+
+  const updateItemQuantity = (item: ItemType, quantity: number) => {
+    const newItems = items.map((i) => {
+      if (i.name === item.name) {
+        return { ...i, quantity };
+      }
+      return i;
+    });
+    setItems(newItems);
+  };
+
+  const total = items.reduce((acc, item) => {
+    return acc + item.price * item.quantity;
+  }, 0);
+
   return (
     <div className="flex justify-center">
-      <main className="w-full max-w-2xl">
-        {items.map((item) => (
-          <Item item={item} key={item.name} />
+      <main className="w-full max-w-2xl py-2">
+        {Object.entries(groupedItems).map(([category, items]) => (
+          <div key={category}>
+            <h2 className="text-3xl font-bold capitalize">{category}</h2>
+            {items.map((item) => (
+              <Item
+                item={item}
+                updateItemQuantity={updateItemQuantity}
+                key={item.name}
+              />
+            ))}
+          </div>
         ))}
+        <div className="flex justify-end">
+          <h2 className="text-2xl font-bold">Total: ${total}</h2>
+        </div>
       </main>
     </div>
   );
